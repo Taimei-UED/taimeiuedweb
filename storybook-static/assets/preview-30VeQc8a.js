89ed5708e57397bc70c2ebb4786864a27013d489
TomWu <tomwu1229@yeah.net>
2026-06-25 15:19:13 +0800

    chore: ignore storybook-static build output

--- storybook-static/assets/preview-30VeQc8a.js
+++
@@ -1,1 +0,0 @@
-const a={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},layout:"centered",backgrounds:{default:"light",values:[{name:"light",value:"#ffffff"},{name:"gray",value:"#f8fafc"},{name:"dark",value:"#0f172a"}]}}};export{a as default};
